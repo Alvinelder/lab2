2c52792bc6be5b50452eaf87256e681a99d28ae7
Alvinelder <pain.suffering67@gmail.com>
2016-02-23 18:33:57 -0500

    lab 2 it 1050

---
+++ lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Enter info for P1 ");
+            System.Console.WriteLine(" ");
+
+
+            Person P1 = new Person();
+            P1.GetFullnameage();
+            P1.GetMaritalstatus();
+            P1.Spouse.GetFullName();
+
+            System.Console.WriteLine(" ");
+            System.Console.WriteLine(" Enter info for P2 ");
+            System.Console.WriteLine(" ");
+                        
+            Person P2 = new Person();
+            P2.GetFullnameage();
+            P2.GetMaritalstatus();
+            P1.Spouse.GetFullName();
+            /////////////
+            System.Console.WriteLine();
+            System.Console.WriteLine(" Printing Results");
+            System.Console.WriteLine(P1.GetFullName());
+            System.Console.WriteLine(P1.Spouse.GetFullName());
+            System.Console.WriteLine(P2.GetFullName());
+            System.Console.WriteLine(P2.Spouse.GetFullName());
+
+            System.Console.WriteLine("Average Age is " + (Person.SumofAllAges / Person.Count));
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab2/person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Person
+    {
+        public string FirstName, LastName;
+        public int Age;
+        public Person Spouse;
+
+        public static Double SumofAllAges;
+        public static int Count;
+
+        // get full name age string
+        public void GetFullnameage()
+        { System.Console.WriteLine("First Name  ");
+            this.FirstName = System.Console.ReadLine();
+          System.Console.WriteLine("Last Name  ");
+            this.LastName = System.Console.ReadLine();
+          System.Console.WriteLine("Age    ");
+            this.Age  = int.Parse(System.Console.ReadLine());
+            Person.SumofAllAges += this.Age;
+            Person.Count++;
+        }
+        ///////////////////////////////
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName + "   (" + this.Age + ")";
+        }
+        // print name and age string
+        public void PrintNameandAge()
+        {
+            System.Console.WriteLine(this.GetFullName());
+            System.Console.WriteLine(this.Age);
+            SumofAllAges += SumofAllAges + this.Age;
+        }
+        //////////////////
+        public void GetMaritalstatus()
+        {
+            System.Console.Write("are you married? (y/n)   ");
+            if (System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                this.Spouse = new Person();
+                this.Spouse.GetFullnameage();
+                this.Spouse.Spouse = this;
+
+            }
+                }
+
+    }
+}
